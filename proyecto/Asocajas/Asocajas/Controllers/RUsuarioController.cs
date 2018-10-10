a5ec3e577e538a634f7070a1f92b27b36abec65e
Omar osuna <saijaroht@gmail.com>
2018-10-10 10:18:19 -0500

    omar

--- proyecto/Asocajas/Asocajas/Controllers/RUsuarioController.cs
+++ proyecto/Asocajas/Asocajas/Controllers/RUsuarioController.cs
@@ -58,7 +58,7 @@ namespace Asocajas.Controllers
 
         public void UpdateTry(RUsuario rsuario)
         {
-            var obj = this.objDb.Update(rsuario);
+            var obj = this.objDb.AddUpdate(rsuario, "IdUsuario");
 
         }
 
