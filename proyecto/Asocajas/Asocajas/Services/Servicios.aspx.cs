89a66e991518ef22369fd6de49261e2aef56e2bf
Omar osuna <saijaroht@gmail.com>
2018-10-12 11:19:22 -0500

    3

--- proyecto/Asocajas/Asocajas/Services/Servicios.aspx.cs
+++ proyecto/Asocajas/Asocajas/Services/Servicios.aspx.cs
@@ -57,7 +57,6 @@ namespace Asocajas.Services
             DataResult jsonData = new DataResult();
             try
             {
-                //DownloadFile();
                 User = UserData;
                 jsonData.Message = UserData;
                 jsonData.Ok = true;
@@ -101,27 +100,5 @@ namespace Asocajas.Services
             set { HttpContext.Current.Session[SESSION_VAR] = value; }
         }
         #endregion
-
-
-        public static void DownloadFile()
-        {
-            //// This is an array of strings right?
-            //string[,] DataToExportToCSV = new string[,] { { "2000", "2" }, { "2001", "4" } };
-
-            //// Use a StringBuilder to accumulate your output
-            //StringBuilder sb = new StringBuilder("Date;C1\r\n");
-            //for (int i = 0; i <= array.GetUpperBound(0); i++)
-            //{
-            //    for (int j = 0; j <= array.GetUpperBound(1); j++)
-            //    {
-            //        sb.Append((j == 0 ? "" : ";") + array[i, j]);
-            //    }
-            //    sb.AppendLine();
-            //}
-
-            //// Write everything with a single command 
-            //File.WriteAllText(@"C:\\FilesCSV\\fileadress.csv", sb.ToString());
-
-        }
     }
 }
