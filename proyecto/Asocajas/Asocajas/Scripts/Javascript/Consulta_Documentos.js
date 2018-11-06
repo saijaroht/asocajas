c3125f67bccc85a72331f8e18e812c393de4aab1
Omar osuna <saijaroht@gmail.com>
2018-11-06 14:04:25 -0500

    consulta document

--- proyecto/Asocajas/Asocajas/Scripts/Javascript/Consulta_Documentos.js
+++ proyecto/Asocajas/Asocajas/Scripts/Javascript/Consulta_Documentos.js
@@ -1,18 +1,32 @@
 ﻿$(document).ready(function () {
-
+    consultaDocumento();
+    debugger;
     
 });
 
-function alerta() {
-    ShowMessage("Mensaje Sistema", "El número de documento no existe en el sistema", "Alerta");
+function consultaDocumento()
+{
+    debugger;
+    consumirServicio(ServiceUrl + "Home/GetCedula?Cedula=1000397409", null, function (dataUsuario) {
+        
+        if (dataUsuario.datosCedulas.nuip)
+        { abrirModal(); }
+        else
+        { alerta();}
+    });
+    
+}
 
-}
-function abrirModal() {
-    var Consultar = $("#btnConsultar");
-    Consultar.click(function () {
-        $('#ModalConsultarUsuarioDocumento').modal('show');
-    })
-}
+    function alerta() {
+        ShowMessage("Mensaje Sistema", "El número de documento no existe en el sistema", "Alerta");
+
+    }
+    function abrirModal() {
+        var Consultar = $("#btnConsultar");
+        Consultar.click(function () {
+            $('#ModalConsultarUsuarioDocumento').modal('show');
+        })
+    }
 
 
 
